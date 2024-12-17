b40705d65752ae989b8a9b9934c88527e7a5db98
Jérôme Danthinne <jerome.danthinne@smile.eu>
2024-12-17 12:06:07 +0100

    fix: move @MainActor annotation to classes

--- Sources/SwiftPOMTesting/BasePage.swift
+++ Sources/SwiftPOMTesting/BasePage.swift
@@ -1,7 +1,7 @@
 import Foundation
 import XCTest
 
-open class BasePage {
+@MainActor open class BasePage {
     let app: XCUIApplication
 
     public init(_ app: XCUIApplication) {
@@ -15,7 +15,7 @@ open class BasePage {
     // MARK: - XCUIElement
 
     /// Function that retrieves an element of type Button
-    @MainActor public func getButton(_ locator: UIElementDescriptor,
+    public func getButton(_ locator: UIElementDescriptor,
                                      timeout: TimeInterval = 10,
                                      in element: XCUIElement? = nil) throws -> XCUIElement
     {
@@ -23,44 +23,44 @@ open class BasePage {
     }
 
     /// Function that retrieves an element in the TabBar of type Button
-    @MainActor public func getTabBarButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getTabBarButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.tabBars.buttons[locator.getId()])
     }
 
     /// Function that retrieves an element of type TextField
-    @MainActor public func getTextField(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getTextField(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.textFields[locator.getId()])
     }
 
     /// Function that retrieves an element of type SecureTextField
-    @MainActor public func getSecureTextField(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getSecureTextField(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.secureTextFields[locator.getId()])
     }
 
     /// Function that retrieves an element of type Text
-    @MainActor public func getStaticText(_ locator: UIElementDescriptor,
+    public func getStaticText(_ locator: UIElementDescriptor,
                                          timeout: TimeInterval = 10) throws -> XCUIElement
     {
         try Wait.waitForExistence(element: app.staticTexts[locator.getId()], timeout: timeout)
     }
 
     /// Function that retrieves an element of type Switch
-    @MainActor public func getSwitch(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getSwitch(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.switches[locator.getId()].switches.element(boundBy: 0))
     }
 
     /// Function that retrieves the - element of a Stepper element
-    @MainActor public func getStepperMinusButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getStepperMinusButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.steppers[locator.getId()].buttons.element(boundBy: 0))
     }
 
     /// Function that retrieves the + element of a Stepper element
-    @MainActor public func getStepperPlusButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getStepperPlusButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.steppers[locator.getId()].buttons.element(boundBy: 1))
     }
 
     /// Function that retrieves an element of type Menu
-    @MainActor public func getMenu(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getMenu(_ locator: UIElementDescriptor) throws -> XCUIElement {
         if #available(iOS 16, *) {
             try getStaticText(locator).firstMatch
         } else {
@@ -69,47 +69,47 @@ open class BasePage {
     }
 
     /// Function that retrieves an element of type NavigationBar
-    @MainActor public func getNavigationBar(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getNavigationBar(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.navigationBars[locator.getId()])
     }
 
     /// Function that retrieves an element of type Button in the presented keyboard
-    @MainActor public func getKeyboardButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getKeyboardButton(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.keyboards.buttons[locator.getId()])
     }
 
     /// Function that retrieves an element of type Alert
-    @MainActor public func getAlert(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getAlert(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.alerts[locator.getId()])
     }
 
     /// Function that retrieves the title element of a presented Alert
-    @MainActor public func getAlertTitle(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getAlertTitle(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.alerts.element.staticTexts[locator.getId()])
     }
 
     /// Function to retrieves the first Button of a presented Alert
-    @MainActor public func getAlertButton(index: Int = 0) throws -> XCUIElement {
+    public func getAlertButton(index: Int = 0) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.alerts.scrollViews.otherElements.buttons.element(boundBy: index))
     }
 
     /// Function that retrieves an element of type ActivityIndicator
-    @MainActor public func getActivityIndicator(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getActivityIndicator(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.activityIndicators[locator.getId()])
     }
 
     /// Function that retrieves an element of type PageIndicator
-    @MainActor public func getPageIndicator() throws -> XCUIElement {
+    public func getPageIndicator() throws -> XCUIElement {
         try Wait.waitForExistence(element: app.pageIndicators.element(boundBy: 0))
     }
 
     /// Function that retrieves an element of type CollectionView
-    @MainActor public func getCollectionView(_ locator: UIElementDescriptor) throws -> XCUIElement {
+    public func getCollectionView(_ locator: UIElementDescriptor) throws -> XCUIElement {
         try Wait.waitForExistence(element: app.collectionViews[locator.getId()])
     }
 
     /// Function that retrieves an element of type OtherElement
-    @MainActor public func getOtherElement(_ locator: UIElementDescriptor,
+    public func getOtherElement(_ locator: UIElementDescriptor,
                                            timeout: TimeInterval = 10) throws -> XCUIElement
     {
         try Wait.waitForExistence(element: app.otherElements[locator.getId()], timeout: timeout)
@@ -118,37 +118,37 @@ open class BasePage {
     // MARK: - XCUIElement counts
 
     /// Function that count the number of elements of type Text
-    @MainActor public func getStaticTextCount(matching locator: UIElementDescriptor) -> Int {
+    public func getStaticTextCount(matching locator: UIElementDescriptor) -> Int {
         app.staticTexts.matching(identifier: locator.getId()).count
     }
 
     /// Function that count the number of elements of type Button
-    @MainActor public func getButtonCount(matching locator: UIElementDescriptor) -> Int {
+    public func getButtonCount(matching locator: UIElementDescriptor) -> Int {
         app.buttons.matching(identifier: locator.getId()).count
     }
 
     /// Function that count the number of elements of type OtherElement
-    @MainActor public func getOtherElementsCount(matching locator: UIElementDescriptor) -> Int {
+    public func getOtherElementsCount(matching locator: UIElementDescriptor) -> Int {
         app.otherElements.matching(identifier: locator.getId()).count
     }
 
     // MARK: - Navigation elements
 
     /// Function that retrieves the navigation bar back button element
-    @MainActor public func getBackButton() throws -> XCUIElement {
+    public func getBackButton() throws -> XCUIElement {
         try Wait.waitForExistence(element: app.navigationBars.firstMatch.buttons.element(boundBy: 0))
     }
 
     // MARK: - Utils
 
     /// Function that swipe down to close a presented screen
-    @MainActor public func swipeDownScreen() throws {
+    public func swipeDownScreen() throws {
         let element = try Wait.waitForExistence(element: app.children(matching: .window).element(boundBy: 0))
         element.swipeDown(velocity: .fast)
     }
 
     /// Function that swipe up the screen
-    @MainActor public func swipeUpScreen() throws {
+    public func swipeUpScreen() throws {
         let element = try Wait.waitForExistence(element: app.children(matching: .window).element(boundBy: 0))
         element.swipeUp(velocity: .fast)
     }
